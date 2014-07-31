gnu-mirror-unofficial/gnushogi

commit df11bbcfb7320a53b2dd653e29509bfaa3eb4d09

Make some non-standard commands engine-defined options
The commands 'tsume', 'contempt' and 'hashdepth' are made into XBoard
engine-defined options (the latter into two), so they can be set
interactively through the Engine Settings dialog of the GUI.

gnushogi/commondsp.c
@@ -1648,6 +1648,10 @@ InputCommand(char *command)
         }
         else if (strcmp(s, "protover") == 0)
         {
+            printf("feature option=\"tsume -check 0\"\n");
+            printf("feature option=\"contempt -spin %d -1000 1000\"\n", contempt);
+            printf("feature option=\"Hash-file search depth -spin %d 0 100\"\n", HashDepth);
+            printf("feature option=\"Hash-file move number -spin %d 0 100\"\n", HashMoveLimit);
             printf("feature myname=\"GNU %s %s\" ",
 #ifdef MINISHOGI
                    "MiniShogi",
@@ -1886,6 +1890,14 @@ InputCommand(char *command)
         else if (strcmp(s, "easy") == 0)
         {
             flag.easy = !flag.easy;
+        }
+        else if (strcmp(s, "option") == 0)
+        {
+            sscanf(sx, "option tsume=%hd", &flag.tsume) ||
+            sscanf(sx, "option hash=%hd",  &flag.hash)  ||
+            sscanf(sx, "option Hash-file search depth=%hd", &HashDepth)    ||
+            sscanf(sx, "option Hash-file move number=%hd", &HashMoveLimit) ||
+            sscanf(sx, "option contempt=%hd", &contempt);
         }
         else if (strcmp(s, "tsume") == 0)
         {

commit 2c53c980b7a6a4c779f744dd8dad0432c4a8cdfe

Xboard: declare "Use hash-file" option to allow disabling hash-file.
df11bbcfb7320a53b2dd653e29509bfaa3eb4d09 introduced support for
this option, but failed to declare it.  Changed its name to
something more descriptive as suggested by hgm.

gnushogi/commondsp.c
@@ -1655,6 +1655,7 @@ InputCommand(char *command)
         else if (strcmp(s, "protover") == 0)
         {
             printf("feature option=\"tsume -check 0\"\n");
+            printf("feature option=\"Use hash-file -check %d\"\n", !!flag.hash);
             printf("feature option=\"contempt -spin %d -1000 1000\"\n", contempt);
             printf("feature option=\"Hash-file search depth -spin %d 0 100\"\n", HashDepth);
             printf("feature option=\"Hash-file move number -spin %d 0 100\"\n", HashMoveLimit);
@@ -1902,7 +1903,7 @@ InputCommand(char *command)
             do {
                 if (sscanf(sx, "option tsume=%hd", &flag.tsume) == 1)
                     break;
-                if (sscanf(sx, "option hash=%hd",  &flag.hash) == 1)
+                if (sscanf(sx, "option Use hash-file=%hd", &flag.hash) == 1)
                     break;
                 if (sscanf(sx, "option Hash-file search depth=%hd", &HashDepth) == 1)
                     break;